8c9adcf4a681edc800e0b7c22609a4d7fa74dbf9
rgalindo33 <rgalindo33@hotmail.com>
2014-07-03 23:42:58 +0200

    added movement to the rover

--- lib/rover.rb
+++ lib/rover.rb
@@ -16,10 +16,32 @@ class Rover
     position.orientation = CARDINAL_POINTS[ ( current_orientation_index + 1 ) % 4 ]
   end
 
+  def move
+    movement_method = "move_#{position.orientation}".to_sym
+    
+    send movement_method
+  end
+
 private
   
   def current_orientation_index
     CARDINAL_POINTS.index position.orientation
+  end
+
+  def move_north
+    position.y += 1
+  end
+
+  def move_east
+    position.x += 1
+  end
+
+  def move_south
+    position.y -= 1
+  end
+
+  def move_west
+    position.x -= 1
   end
 
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Rover do
 
-  let( :position ){ Position.new( 1, 0, :north ) }
+  let( :position ){ Position.new( 2, 1, :north ) }
 
   subject{ Rover.new position }
 
@@ -12,33 +12,82 @@ describe Rover do
     end
   end
 
-  # describe '#move' do
-  #   it '' do
-  #     # binding.pry
-  #   end
-  # end
-
   describe '#turn_left' do
-    it 'changes the direction in the proper direction' do
+    it 'changes the direction in the properly' do
       subject.turn_left
+
       expect( subject.position.orientation ).to eq :west
     end
 
   end
 
   describe '#turn_right' do
-    it 'changes the direction in the proper direction' do
+    it 'changes the direction in the properly' do
       subject.turn_right
+
       expect( subject.position.orientation ).to eq :east
     end
 
     it 'successfully turns 360 degrees' do
-      4.times do
-        subject.turn_right
-      end
+      4.times { subject.turn_right }
 
       expect( subject.position.orientation ).to eq :north
     end
   end
+
+  describe '#move' do
+    context 'going north' do
+    
+      let( :final_position ){ Position.new( 2, 2, :north ) }
+
+      it 'moves on the right direction' do
+        subject.move
+
+        expect( subject.position ).to eq final_position
+      end
+      
+    end
+
+    context 'going east' do
+    
+      let( :final_position ){ Position.new( 3, 1, :east ) }
+
+      it 'moves on the right direction' do
+        subject.turn_right
+        subject.move
+
+        expect( subject.position ).to eq final_position
+      end
+      
+    end
+
+    context 'going south' do
+    
+      let( :final_position ){ Position.new( 2, 0, :south ) }
+
+      it 'moves on the right direction' do
+        2.times{ subject.turn_right }
+        subject.move
+
+        expect( subject.position ).to eq final_position
+      end
+      
+    end
+
+    context 'going west' do
+    
+      let( :final_position ){ Position.new( 1, 1, :west ) }
+
+      it 'moves on the right direction' do
+        subject.turn_left
+        subject.move
+
+        expect( subject.position ).to eq final_position
+      end
+      
+    end
+
+  end
+
 
 end
