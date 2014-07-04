a10714a4dbbbaa5dabf30c11ded943b93ae530f1
rgalindo33 <rgalindo33@hotmail.com>
2014-07-04 17:48:09 +0200

    navigator class ready

--- lib/navigator.rb
+++ lib/navigator.rb
@@ -1,2 +1,20 @@
 class Navigator
+
+  attr_reader :plateu, :rover, :instructions
+
+  def initialize plateu, rover, instructions=[]
+    @plateu       = plateu
+    @rover        = rover
+    @instructions = instructions
+  end
+
+  def go_explore
+    instructions.each do |instruction|
+      rover.send instruction
+
+      raise "Ground control to Major Tom - Rover out of bounds" if not plateu.valid_position? rover.position
+    end
+  end
+
+
 end
--- spec/navigator_spec.rb
+++ spec/navigator_spec.rb
@@ -2,4 +2,49 @@ require 'spec_helper'
 
 describe Navigator do
 
+  subject{ Navigator.new plateu, rover, instructions }
+
+  let( :plateu ){ Plateu.new [ 5, 5 ] }
+  let( :rover ) { Rover.new Position.new( 1, 2, :north ) }
+  
+  let( :instructions ) do
+   [ 
+    :turn_left, :move,
+    :turn_left, :move,
+    :turn_left, :move,
+    :turn_left, :move,
+    :move
+  ]
+  end
+
+  describe '#initialize' do
+    it 'sets the navigation environment' do
+      expect( subject.plateu ).to eq plateu
+      expect( subject.rover ).to eq rover
+      expect( subject.instructions ).to eq instructions
+    end
+  end
+
+  describe '#go_explore' do
+
+    let( :expected_position ){ Position.new( 1, 3, :north ) }
+
+    it 'explores the plateu accordint to its instructions' do
+      subject.go_explore
+
+      expect( rover.position ).to eq expected_position
+    end
+
+    context 'going out of bouds' do
+      
+      let( :instructions ){ [ :turn_right, :turn_right, :move, :move ] }
+
+      it 'raises an exception if rover is out of bounds' do
+
+        expect{ subject.move }.to raise_error
+      end
+    end
+
+  end
+
 end
